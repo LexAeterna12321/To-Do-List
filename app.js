c02dfc1f04c3d8f3ae28e76590b4a8bc84dd4700
LexAeterna12321 <lexaeterna12321@gmail.com>
2018-10-01 20:05:34 +0200

    scope

--- app.js
+++ app.js
@@ -1,49 +1,50 @@
- const listInput = document.querySelector(".container__input");
- listInput.addEventListener("keydown", e => {
-     // VARS
-     let listInputValue = listInput.value;
-     const toDoList = document.querySelector(".main__to-do");
-     const doneList = document.querySelector(".main__done");
-     const listItem = document.createElement("li");
-     const controlOk = document.createElement("div");
-     const controlRemove = document.createElement("div");
-     const toDoListItems = [];
-     const doneListItems = [];
+const toDoListItems = [];
+const doneListItems = [];
+const listInput = document.querySelector(".container__input");
+listInput.addEventListener("keydown", e => {
+    // VARS
+    let listInputValue = listInput.value;
+    const toDoList = document.querySelector(".main__to-do");
+    const doneList = document.querySelector(".main__done");
+    const listItem = document.createElement("li");
+    const controlOk = document.createElement("div");
+    const controlRemove = document.createElement("div");
 
-     // input on enter confirmation
-     if (e.keyCode == 13) {
 
-         if (listInputValue === "") {
-             alert(`Please enter a task`);
-         } else {
-             // adding list items
-             toDoList.appendChild(listItem).classList.add("main__to-do__item");
-             listItem.textContent = `- ${listInputValue}`;
-             // 
-             // pushing values to array
-             toDoListItems.push(listInputValue);
-             // 
-             // adding controls
-             listItem.appendChild(controlOk).classList.add("main__control--ok");
-             controlOk.textContent = "OK";
-             listItem.appendChild(controlRemove).classList.add("main__control--remove");
-             controlRemove.textContent = "X";
-             //
-         }
-     }
+    // input on enter confirmation
+    if (e.keyCode == 13) {
 
-     //   controls management
-     controlOk.addEventListener("click", () => {
-         doneList.appendChild(listItem).classList.add("main__done__item");
-         doneListItems.push(toDoListItems.pop());
-         listItem.removeChild(controlOk);
-     })
-     controlRemove.addEventListener("click", () => {
-         listItem.style.textDecoration = "line-through";
-         controlRemove.addEventListener("click", () => {
-             listItem.classList.add("fadingItem");
-             setTimeout(() => listItem.remove(), 1000);
-         })
+        if (listInputValue === "") {
+            alert(`Please enter a task`);
+        } else {
+            // adding list items
+            toDoList.appendChild(listItem).classList.add("main__to-do__item");
+            listItem.textContent = `- ${listInputValue}`;
+            // 
+            // pushing values to array
+            toDoListItems.push(listInputValue);
+            // 
+            // adding controls
+            listItem.appendChild(controlOk).classList.add("main__control--ok");
+            controlOk.textContent = "OK";
+            listItem.appendChild(controlRemove).classList.add("main__control--remove");
+            controlRemove.textContent = "X";
+            //
+        }
+    }
 
-     })
- })
+    //   controls management
+    controlOk.addEventListener("click", () => {
+        doneList.appendChild(listItem).classList.add("main__done__item");
+        doneListItems.push(toDoListItems.pop());
+        listItem.removeChild(controlOk);
+    })
+    controlRemove.addEventListener("click", () => {
+        listItem.style.textDecoration = "line-through";
+        controlRemove.addEventListener("click", () => {
+            listItem.classList.add("fadingItem");
+            setTimeout(() => listItem.remove(), 1000);
+        })
+
+    })
+})
